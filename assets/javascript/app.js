114726dc933974004f2c22fb4d08dd1d1b992cbe
petercarmines <34289206+petercarmines@users.noreply.github.com>
2018-02-27 07:10:25 -0500

    basic working outline

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,81 @@
-$(document).ready(function() {
-
-
-number=7;
+var number=10;
 var intervalId;
-
 var rightAnswers = 0;
 var wrongAnswers = 0;
 
-function countDown() {
-    number--;
-    $("#timer").html("<h2>" + number + "</h2>");
-    if (number === 0) {
-    clearInterval(intervalId);
-    alert("Time's up!")
+var questions = ["What is my name?", "What is my age?", "How much do I love programming?"]
+var choices = ["Pete", "Jeff", "Matt", "28", "25", "27", "a little","it's okay", "i love it!!"]
+var correctAnswer = ["a", "b", "c"]
+
+function write() {
+    number=10;
+    $("#timer").html("Question Timer: " + number);
+    $("#question").html(questions[0]);
+    $("#a").text(choices[0]);
+    $("#b").text(choices[1]);
+    $("#c").text(choices[2]);
+    $("#message, #right, #wrong").empty();
+    if (questions.length >= 1) {
+    runq();
+    } else {
+    results();
     }
 }
 
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(countDown, 1000);
-  }
+function results () {
+    $("#right").html("Correct Answers: " + rightAnswers);
+    $("#message").html("That's all for now! Here's how you did:");
+    $("#wrong").html("Incorrect Answers: " + wrongAnswers);
+    $("#timer").empty();
+}
 
-if($('#correct1').is(':checked')) { 
-    rightAnswers = rightAnswers + 1; }
-
-run();
-
-
-
-$('#submit').click(function() {
-   clearInterval(intervalId);
-   $("#wins").html(rightAnswers);
-
+function checker () {            
+    $("#a, #b, #c").on('click', function(){
+        value = $(this).attr('name');
+            if (value === correctAnswer[0]) {
+                rightAnswers = rightAnswers + 1;
+                resetQ(); 
+                $("#message").html("That's right!");
+            } else {
+                wrongAnswers = wrongAnswers + 1;
+                resetQ(); 
+                $("#message").html("Sorry! That is Incorrect!");
+            }              
     })
+}
+      
+function resetQ () {
+    $("#a, #b, #c, #question, #timer, #right, #wrong").empty();
+    clearInterval(intervalId);
+    questions.shift(0);
+    choices.splice(0, 3);
+    correctAnswer.shift(0);
+    setTimeout(write, 3000);
+} 
+            
+function countDownq() {
+    number--;
+    $("#timer").html("Question Timer: " + number);
+        if (number === 0) {
+            clearInterval(intervalId);
+            wrongAnswers = wrongAnswers + 1;
+            alert("Outta time!")
+            resetQ();
+        }
+}
+                
+function runq() {
+    clearInterval(intervalId);
+    intervalId = setInterval(countDownq, 1000);
+}
+                   
+$(document).ready(function () {
+    $("#start").on("click", function(){
+        $("#start").remove();
+        write ();
+        checker ();
+    });          
+});
+         
 
-})
 
-
-   
